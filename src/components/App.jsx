2fc099dcef9319d17253a60e00d7f3aeb1c4924a
Elena Tatsenko <lena.harchenko24@gmail.com>
2022-09-29 15:34:50 +0300

    add Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import Searchbar from "./Searchbar/Searchbar";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//const KEY = "29216900-862a9e8f9f3ad454828049dde";
+
+
+export default class App extends Component {
+  state = {
+    imageName: '',
+  }
+  handleFormSubmit = imageName => this.setState({ imageName });
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit}></Searchbar>
+        <ToastContainer />
+      
+    </>
   );
+  }
+  
 };
